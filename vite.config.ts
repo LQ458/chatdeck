7fc3766b2a92bb992b293bab029b7dba0def1ad5
Leo Yihao Qin <yihao.qin17311-biph@basischina.com>
2024-12-22 14:36:57 +0800

    fix: server env reading

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,53 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          ["@babel/plugin-transform-react-jsx", { runtime: "automatic" }],
-        ],
-      },
-    }),
-  ],
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
-  build: {
-    target: "es2015",
-    cssCodeSplit: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    base: "./",
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            ["@babel/plugin-transform-react-jsx", { runtime: "automatic" }],
+          ],
+        },
+      }),
+    ],
+    optimizeDeps: {
+      exclude: ["lucide-react"],
     },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        configure: (proxy) => {
-          proxy.on("error", (err) => {
-            console.log("🔥 proxy error", err);
-          });
-          proxy.on("proxyReq", (proxyReq) => {
-            console.log("🔄 Sending Request to:", proxyReq.path);
-          });
-          proxy.on("proxyRes", (proxyRes) => {
-            console.log("✨ Received Response from:", proxyRes.url);
-          });
+    build: {
+      target: "es2015",
+      cssCodeSplit: false,
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
         },
       },
     },
-  },
+    server: {
+      port: parseInt(env.VITE_PORT || '5173'),
+      proxy: {
+        "/api": {
+          target: `http://localhost:${env.PORT}`,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+          configure: (proxy) => {
+            proxy.on("error", (err) => {
+              console.log("🔥 proxy error", err);
+            });
+            proxy.on("proxyReq", (proxyReq) => {
+              console.log("🔄 Sending Request to:", proxyReq.path);
+            });
+            proxy.on("proxyRes", (proxyRes) => {
+              console.log("✨ Received Response from:", proxyRes.url);
+            });
+          },
+        },
+      },
+    },
+  };
 });
